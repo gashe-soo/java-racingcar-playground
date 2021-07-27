2aeccdefb601e0a0202ea8e81d739ba48269f0c1
gashe-soo <gashe7@gmail.com>
2021-07-27 15:32:12 +0900

    refactor:convention에 맞게 formatting

--- src/main/java/racing/Racing.java
+++ src/main/java/racing/Racing.java
@@ -17,10 +17,12 @@ public class Racing {
     }
 
     private void validateRound(int totalRound) {
-        if (totalRound < ROUND_LOW)
+        if (totalRound < ROUND_LOW) {
             throw new IllegalArgumentException("라운드는 1회 이상이어야 합니다.");
-        if (totalRound > ROUND_HI)
+        }
+        if (totalRound > ROUND_HI) {
             throw new IllegalArgumentException("라운드는 10회 이하이어야 합니다.");
+        }
     }
 
 
